218c33862cd2253f02a816b617cf20b14f500834
JinZhaolu <1756404649@qq.com>
2024-02-24 22:48:18 +0800

    fix: migrate to v2

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\jin17\flutter"
-export "FLUTTER_APPLICATION_PATH=c:\Users\jin17\DevelopmentProjects\PasswordManager\flutter2\packages\nesp_sdk_flutter_old\nesp_sdk\example"
+export "FLUTTER_ROOT=C:\Users\jin17\fvm\versions\3.19.0"
+export "FLUTTER_APPLICATION_PATH=c:\Users\jin17\DevelopmentProjects\PasswordManager\flutter\packages\nesp_sdk_flutter_old\nesp_sdk\example"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
